ae68dd0606089f7379a4b7aae4423faa7890978e
Sofia096 <sofiacat20062508@gmail.com>
2022-05-09 15:52:21 +0300

    Готова первая страница. Начало работы с добавлением в избраное.

--- main.py
+++ main.py
@@ -71,18 +71,11 @@ def login():
 
 @app.route('/cat', methods=['GET', 'POST'])
 def cat():
-    form = LoginForm()
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        user = db_sess.query(User).filter(User.email == form.email.data).first()
-        if user and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            return redirect("/")
-        return render_template('login.html',
-                               message="Неправильный логин или пароль",
-                               form=form)
-    return render_template('login.html', title='Авторизация', form=form)
+        return render_template('Cats.html', title='Меню кошки')
 
+@app.route('/abisin', methods=['GET', 'POST'])
+def abisin():
+        return render_template('Abisin.html', title='Абиссинская кошка')
 
 
 if __name__ == '__main__':
